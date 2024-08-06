6ad1384c19ee0e9cf9121b66ec313357c8a30e36
shymaaelhawary1 <150074004+shymaaelhawary1@users.noreply.github.com>
2024-08-06 20:21:44 +0300

    onbourding add

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,42 @@
+import 'package:chanzel_app/OnbourdingScreen.dart';
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:device_preview/device_preview.dart';
+import 'home.dart';
 
 void main() {
   runApp(
     DevicePreview(
-      builder: (context) => const MyApp(),
+      builder: (context) => MyApp(),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       builder: DevicePreview.appBuilder,
-      home: HomeScreen(),
+      home: FutureBuilder<bool>(
+        future: _isOnboardingComplete(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: const Center(child: CircularProgressIndicator()),
+            );
+          } else if (snapshot.data == true) {
+            return HomeScreen();
+          } else {
+            return const OnboardingScreen();
+          }
+        },
+      ),
       useInheritedMediaQuery: true,
     );
+  }
+
+  Future<bool> _isOnboardingComplete() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('onboarding_complete') ?? false;
   }
 }
